f6647fc1747730bf23018fac08d62dbb6a6d8def
Jonskadev <juan.olguin.13@sansano.usm.cl>
2017-12-20 05:44:14 -0300

    header y footer listos

---
+++ resources/views/Home.blade.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Gimnasio III</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="/css/normalize.css">
+        <link rel="stylesheet" href="/css/Home.css">
+
+    </head>
+    <body>
+        <header>
+            <figure id="usm">
+                <img src="/images/usm.png">
+            </figure>
+            <h1>Gimnasio III USM</h1>
+            <figure id="gym">
+                <img src="/images/gym.png">
+            </figure>
+        </header>
+        <nav>
+
+        </nav>
+        <!--
+        <section>
+            <h1>
+                Gimnasio de Máquinas, Pesas y Aeróbica
+            </h1>
+            <div id="right">
+
+            </div>
+            <div id="center">
+
+            </div>
+            <div id="left">
+
+            </div>
+        </section>
+        -->
+        <footer>
+            <h1>© USM</h1>
+        </footer>
+    </body>
+</html>
